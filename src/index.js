eecb74108b2eff327b5eae2df9a4261f6e63fadf
liu9293 <mylock.kai@gmail.com>
2018-07-13 13:55:16 +0800

    correct chain id

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class HDNode {
         _privateKey: privateKey
       }
     }
-    this._chainId = chainId || 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
+    this._chainId = chainId || 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
   }
 
   static generateMnemonic () {
